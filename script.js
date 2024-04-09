0f5c39c3a58dcbf31b2435c182f8ad0340c53413
pechenka19 <maxim.max.n.v@gmail.com>
2024-04-09 22:07:51 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function changePassText(item) {
+    let variable = document.querySelector("." + item);
+    if (variable.type === "password"){
+        variable.type = "text"
+    }
+    else variable.type = "password"
+}
+
+document.getElementById('form__centre__login').addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log(event.elements)
+    let nameLine = document.querySelector('.nameInputMain__login').value
+    document.querySelector('.up__sign__centre__login').innerHTML = nameLine + ". HI!"
+    document.querySelector('.form__centre__login').style.display = 'none'
+    document.querySelector('.registerRef__centre__login').style.display = 'none'
+});
